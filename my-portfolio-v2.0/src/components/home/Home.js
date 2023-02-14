b7fe97a3a7315244e7172e061b5be844e7bc705a
Bruno Souza <brunoalksouza@gmail.com>
2023-02-14 17:32:53 -0300

    first commit

--- my-portfolio-v2.0/src/components/home/Home.js
+++ my-portfolio-v2.0/src/components/home/Home.js
@@ -1,5 +1,49 @@
+import { FiSearch } from "react-icons/fi";
+import "./Home.css";
+import { useState } from "react";
+import api from "../../services/api";
+
 function Home() {
-  return <h1>Hello World</h1>;
+  const [input, setInput] = useState("");
+
+  async function handleSearch() {
+    if (input == 0 || input.length < 8 || input.length > 8) {
+      alert("Digite um CEP valido");
+      return;
+    }
+    try{
+      const response = await api.get(`${input}/json/`);
+      const data = await response.json();
+      console.log(data);
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button type="button" className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="FFF" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 37514000</h2>
+        <span>Rua Teste</span>
+        <span>Complemento</span>
+        <span>Bairro</span>
+        <span>Campo Grande - MS</span>
+      </main>
+    </div>
+  );
 }
 
 export default Home;

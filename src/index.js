6f50c19f172fb5b89f2841268574ef5c6e886dc9
Bruno Souza <brunoalksouza@gmail.com>
2023-04-10 10:09:15 -0300

    added-paste-app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/home/Home';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
